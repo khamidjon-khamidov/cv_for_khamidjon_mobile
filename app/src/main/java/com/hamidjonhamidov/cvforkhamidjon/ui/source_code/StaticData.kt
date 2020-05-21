5d51e14b498102454afaeb584b6ec625867135d8
SomeoneAndNoone <hamidovhamid1998@gmail.com>
2020-05-21 12:14:38 +0500

    source code ui done

---
+++ app/src/main/java/com/hamidjonhamidov/cvforkhamidjon/ui/source_code/StaticData.kt
@@ -0,0 +1,40 @@
+package com.hamidjonhamidov.cvforkhamidjon.ui.source_code
+
+import com.hamidjonhamidov.cvforkhamidjon.R
+import com.hamidjonhamidov.cvforkhamidjon.ui.source_code.custom_treeview.PathFile
+
+object StaticData {
+
+    val leafFileList = listOf(
+        PathFile(listOf("whoiskhamidjon", "di", "blog_posts", "BlogFragmentBuildersModule"), R.raw.aboutappfragment),
+        PathFile(listOf("whoiskhamidjon", "di", "blog_posts", "BlogModule"), R.raw.personalinfo),
+        PathFile(listOf("whoiskhamidjon", "di", "blog_posts", "BlogPostsScope"), R.raw.postsdependencyprovider),
+        PathFile(listOf("whoiskhamidjon", "hello", "blog_posts"), R.raw.requestfromfirebase),
+        PathFile(listOf("whoiskhamidjon", "hids", "blog_posts"), R.raw.postsdatastatechangelistener),
+        PathFile(listOf("I am khamidjon", "asdfa", "blog_posts", "BlogPostsScope"), R.raw.aboutmemodel)
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
